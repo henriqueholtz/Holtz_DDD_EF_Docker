8800f085e2f5ece65f984167792583e9e97970d4
henriqueholtz <henrique_holtz@hotmail.com>
2021-11-04 21:53:03 -0300

    Implemented Repositories: Products and Customers

---
+++ Holtz_DDD_EF_Docker.Infraestructure/Repositories/RepositoryCustomers.cs
@@ -0,0 +1,15 @@
+﻿using Holtz_DDD_EF_Docker.Domain.Core.Interfaces.Repositories;
+using Holtz_DDD_EF_Docker.Domain.Entities;
+using Holtz_DDD_EF_Docker.Infraestructure.Data;
+
+namespace Holtz_DDD_EF_Docker.Infraestructure.Repositories
+{
+    public class RepositoryCustomers : RepositoryBase<Customer>, IRepositoryCustomers
+    {
+        public readonly ApplicationContext _context;
+        public RepositoryCustomers(ApplicationContext applicationContext) : base(applicationContext)
+        {
+            _context = applicationContext;
+        }
+    }
+}
---
+++ Holtz_DDD_EF_Docker.Infraestructure/Repositories/RepositoryProducts.cs
@@ -0,0 +1,15 @@
+﻿using Holtz_DDD_EF_Docker.Domain.Core.Interfaces.Repositories;
+using Holtz_DDD_EF_Docker.Domain.Entities;
+using Holtz_DDD_EF_Docker.Infraestructure.Data;
+
+namespace Holtz_DDD_EF_Docker.Infraestructure.Repositories
+{
+    public class RepositoryProducts : RepositoryBase<Product>, IRepositoryProducts
+    {
+        private readonly ApplicationContext _context;
+        public RepositoryProducts(ApplicationContext applicationContext) : base(applicationContext)
+        {
+            _context = applicationContext;
+        }
+    }
+}
