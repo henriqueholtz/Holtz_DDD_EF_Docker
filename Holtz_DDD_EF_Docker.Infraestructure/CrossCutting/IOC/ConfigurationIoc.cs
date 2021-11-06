86398bad86825c0b58d8e512719c7c774193d836
henriqueholtz <henrique_holtz@hotmail.com>
2021-11-05 21:01:35 -0300

    Added ConfigurationIoc and ModuleIoc (to dependency injection with Autofac)

---
+++ Holtz_DDD_EF_Docker.Infraestructure/CrossCutting/IOC/ConfigurationIoc.cs
@@ -0,0 +1,40 @@
+﻿using Autofac;
+using Holtz_DDD_EF_Docker.Application;
+using Holtz_DDD_EF_Docker.Application.Interfaces;
+using Holtz_DDD_EF_Docker.Application.Interfaces.Mappers;
+using Holtz_DDD_EF_Docker.Application.Map;
+using Holtz_DDD_EF_Docker.Domain.Core.Interfaces.Repositories;
+using Holtz_DDD_EF_Docker.Domain.Core.Interfaces.Services;
+using Holtz_DDD_EF_Docker.Domain.Services;
+using Holtz_DDD_EF_Docker.Infraestructure.Data.Repositories;
+
+namespace Holtz_DDD_EF_Docker.Infraestructure.CrossCutting.IOC
+{
+    public class ConfigurationIoc
+    {
+        public static void Load(ContainerBuilder builder)
+        {
+            #region IOC
+            // Application Services
+            builder.RegisterType<ApplicationServiceCustomer>().As<IApplicationServiceCustomers>();
+            builder.RegisterType<ApplicationServiceProduct>().As<IApplicationServiceProducts>();
+
+
+            // Services
+            builder.RegisterType<ServiceCustomers>().As<IServiceCustomers>();
+            builder.RegisterType<ServiceProducts>().As<IServiceProducts>();
+
+
+            // Repositories
+            builder.RegisterType<RepositoryCustomers>().As<IRepositoryCustomers>();
+            builder.RegisterType<RepositoryProducts>().As<IRepositoryProducts>();
+
+
+            //Mappers
+            builder.RegisterType<MapperCustomer>().As<IMapperCustomer>();
+            builder.RegisterType<MapperProduct>().As<IMapperProduct>();
+
+            #endregion
+        }
+    }
+}
---
+++ Holtz_DDD_EF_Docker.Infraestructure/CrossCutting/IOC/ModuleIoc.cs
@@ -0,0 +1,12 @@
+﻿using Autofac;
+
+namespace Holtz_DDD_EF_Docker.Infraestructure.CrossCutting.IOC
+{
+    public class ModuleIoc : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            ConfigurationIoc.Load(builder);
+        }
+    }
+}
