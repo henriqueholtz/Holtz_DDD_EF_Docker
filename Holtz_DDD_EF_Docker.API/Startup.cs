d6d26083feadb315096685e73b518f548f7cc550
henriqueholtz <henrique_holtz@hotmail.com>
2021-11-05 21:24:34 -0300

    Configure depency injection into startup and program (by API) with Autofac + AddDbContext into Startup (from appsettings.Development.json)

--- Holtz_DDD_EF_Docker.API/Startup.cs
+++ Holtz_DDD_EF_Docker.API/Startup.cs
@@ -1,16 +1,14 @@
+using Autofac;
+using Holtz_DDD_EF_Docker.Infraestructure.CrossCutting.IOC;
+using Holtz_DDD_EF_Docker.Infraestructure.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Holtz_DDD_EF_Docker.API
 {
@@ -26,12 +24,21 @@ namespace Holtz_DDD_EF_Docker.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            string connectionString = Configuration["SqlConnection:Default"];
+            services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(connectionString));
 
             services.AddControllers();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Holtz_DDD_EF_Docker.API", Version = "v1" });
             });
+        }
+
+        public void ConfigureContainer(ContainerBuilder builder)
+        {
+            builder.RegisterModule(new ModuleIoc());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
