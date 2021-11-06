a3eda3403b5d5357595a977c34ae7c662163c7fe
henriqueholtz <henrique_holtz@hotmail.com>
2021-11-05 21:37:05 -0300

    Add CustomerController (with Get, GetAll, Add, Update, Remove) respectively the methods: (GET, GET, POST, PUT, DELETE)

---
+++ Holtz_DDD_EF_Docker.API/Controllers/CustomerController.cs
@@ -0,0 +1,82 @@
+﻿using Holtz_DDD_EF_Docker.Application.Dtos;
+using Holtz_DDD_EF_Docker.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace Holtz_DDD_EF_Docker.API.Controllers
+{
+    [Route("customer/")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly IApplicationServiceCustomers _applicationServiceCustomers;
+        public CustomerController(IApplicationServiceCustomers applicationServiceCustomers)
+        {
+            _applicationServiceCustomers = applicationServiceCustomers;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return Ok(_applicationServiceCustomers.GetAll());
+        }
+
+
+        [HttpGet("{id}")]
+        public ActionResult<IEnumerable<string>> Get(int id)
+        {
+            return Ok(_applicationServiceCustomers.GetById(id));
+        }
+
+        [HttpPost]
+        public ActionResult Add([FromBody] CustomerDto dto)
+        {
+            try
+            {
+                if (dto == null)
+                    return NoContent();
+
+                _applicationServiceCustomers.Add(dto);
+                return Ok("Customer added succesfully!");
+
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPut]
+        public ActionResult Update([FromBody] CustomerDto dto)
+        {
+            try
+            {
+                if (dto == null)
+                    return NoContent();
+
+                _applicationServiceCustomers.Update(dto);
+                return Ok("Customer updated succesfully!");
+
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Remove(int id)
+        {
+            try
+            {
+                _applicationServiceCustomers.Remove(id);
+                return Ok("Customer removed succesfully!");
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
